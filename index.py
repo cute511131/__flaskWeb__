1ba85f585372cfa50a14f49b3c93ecf615038aaa
cute511131 <cute511131@gmail.com>
2023-10-05 09:17:01 +0800

    111

--- index.py
+++ index.py
@@ -5,8 +5,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
-
-@app.route("/hellow_world")
-def hello_world():
-    return"<h1>Hello! world.</h1>"
+    return render_template("homework.html")
